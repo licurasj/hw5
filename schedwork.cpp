8006a0e68b50552c74dcb10a0a86ae6d12ba5ad9
Yonghao Jin <lucasyonghao@outlook.com>
2024-04-20 16:34:25 -0700

    finished hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,8 +21,16 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
-
+bool scheduleHelper(
+    AvailabilityMatrix& avail,
+    DailySchedule& sched,
+    size_t numDays, 
+    size_t numWorker,
+    size_t numMaxShift,
+    size_t numNeeded,
+    size_t row,size_t col,
+    vector<size_t>& vM
+);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -37,9 +45,70 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    size_t numDays = avail.size();
+    size_t numWorker = avail[0].size();
+    vector<size_t> vM(numWorker,0);
+    AvailabilityMatrix availCpy = avail;
 
-
+    for(size_t i =0; i<numDays;i++){
+        vector<Worker_T> temp;
+        sched.push_back(temp);
+    }
+    return scheduleHelper(
+        availCpy, sched,
+        numDays, numWorker, maxShifts, dailyNeed,
+        0, 0, vM
+    );
 
 
 }
 
+bool scheduleHelper(
+    AvailabilityMatrix& avail,
+    DailySchedule& sched,
+    size_t numDays, 
+    size_t numWorker,
+    size_t numMaxShift,
+    size_t numNeeded,
+    size_t row,size_t col,
+    vector<size_t>& vM
+)
+{
+    if(row==numDays){
+        return true;
+    }
+    if(col==numNeeded){
+        return scheduleHelper(
+            avail, sched,
+            numDays, numWorker, numMaxShift, numNeeded,
+            row+1, 0, vM
+        );
+    }
+    
+
+    for(size_t i=0; i<numWorker;i++){ //iterate through all workers
+        if (avail[row][i]==1) // if worker available
+        {   
+            
+            if( vM[i]<numMaxShift){
+                vM[i]++;
+                avail[row][i]=0;
+                sched[row].push_back(i);
+
+                if( scheduleHelper(
+                    avail, sched,
+                    numDays, numWorker, numMaxShift, numNeeded,
+                    row, col+1, vM))
+                {
+                    return true;
+                }
+                avail[row][i]=1;
+                vM[i]--;
+                sched[row].pop_back();
+            }
+            
+        }   
+    }
+    
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,7 +14,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(std::set<std::string>& words, std::string in, std::vector<char> floatVector);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,89 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> words; //set to store all words generated
+    std::vector<char> floatVector(floating.begin(),floating.end());
 
+    wordleHelper(words, in, floatVector);
+    
+    std::set<std::string>::iterator it=words.begin();
+    /*
+    std::set<std::string>::iterator itD=dict.begin();
+    std::set<std::string> out;
+    while(itD!=dict.end()){
+        while(it!=words.end()){
+            if(*it == *itD){
+                out.insert(*it);
+            }
+            it++;
+        }
+        itD++;
+    }
+    */
+    
+    while(it!=words.end()){
+        if(dict.find(*it)== dict.end()){
+            it = words.erase(it);
+            continue;
+        }
+        it++;
+    }
+    
+    return words;
 }
 
 // Define any helper functions here
+void wordleHelper(std::set<std::string>& words, std::string in, std::vector<char> floatVector){
+    
+    size_t spaceOpen = 0; //number of spaces open in the string in
+    int ind = -1; //index number of in to be worked on for this iteration
+    for(size_t i=0; i<in.size();i++){
+        if (in[i]=='-'){
+            if(ind==-1){
+                ind = i;
+            }
+            spaceOpen++;
+        }
+    }
+
+    //check if all spaces filled
+    if(spaceOpen==0){ //word complete
+        words.insert(in);//insert into set
+        return;
+    }
+
+    //check if enough space for floating char
+    bool noSpace = false;
+    if(spaceOpen <= floatVector.size()){ //float size vs remaining space
+        noSpace= true;
+    }
+    
+    std::string alphabet="abcdefghijklmnopqrstuvwxyz";
+    std::string::iterator it;
+
+    for(it=alphabet.begin();it!=alphabet.end();it++){ //loop try all alphabet
+        
+        bool isFloat = false;
+        std::vector<char>::iterator itV = floatVector.begin();
+        while(itV!=floatVector.end()){
+            if(*it==*itV){
+                itV = floatVector.erase(itV);
+                isFloat=true;
+                break;
+            }
+            itV++;
+        }
+        
+        if(noSpace && !isFloat){
+            continue;
+        }
+
+        in[ind]=*it; //set in with character
+        wordleHelper(words, in, floatVector); // recurese
+        
+        if(isFloat){ // reset float and in after trying
+            floatVector.push_back(*it);
+        }
+        in[ind]='-';
+    } 
+}
